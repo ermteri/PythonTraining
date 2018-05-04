7d6d00cea74aed308f75e88fdc254ecc69b53b19
ermteri <ermteri@gmail.com>
2018-05-04 15:38:40 +0200

    fine-tuned REST server

--- rest_api_server.py
+++ rest_api_server.py
@@ -1,6 +1,23 @@
 from bottle import *
 from pprint import pprint
 import time
+import math
+index_success_template='''\
+<html>
+<h1>Welcome to time-presenter and area calculator</h1>
+<p>Current time is: {{time}}</p>
+<p>Area of a circle with radius {{radius}} is {{area}}</p>
+<i>See you around!!</i>
+</html>
+'''
+index_fail_template='''\
+<html>
+<h1>Welcome to time-presenter and area calculator</h1>
+<p>Current time is: {{time}}</p>
+<p>Sorry, the area couldn't be calculated because radius {{radius}}</p>
+<i>See you around!!</i>
+</html>
+'''
 @route('/')
 def welcome():
     print(request.headers.get('Accept'))
@@ -19,8 +36,21 @@ def upper_case_service(word):
 @route('/circle')
 def circle_area():
     d=dict(request.query)
-    r=int(dict(request.query).get('radius'))
-    return dict(radius=str(r),something=dict(area="r*pi^2"),listan=[1,2,3],area=str(3.14*r**2.0))
+    if 'radius' in d.keys():
+       if d.get('radius').replace('.','',1).isdigit():
+           return dict(radius=str(r),something=dict(area="r*pi^2"),listan=[1,2,3],area=str(3.14*r**2.0))
     
+@route('/index')
+def index():
+    d=dict(request.query)
+    t=time.asctime(time.localtime())
+    if 'radius' in d.keys():
+       r=d.get('radius')
+       if r.replace('.','',1).isdigit():
+           a=math.pi*int(r)**2.0
+           return template(index_success_template,time=t,radius=r, area=a)
+       return template(index_fail_template,time=t,radius='was invalid ('+r+')')
+    return template(index_fail_template,time=t,radius='didn\'t exist')
+
 if __name__ == "__main__":
     run(host='localhost',port=8080)
